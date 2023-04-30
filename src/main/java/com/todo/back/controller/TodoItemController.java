ee7eaec209326e6a1f67cea469fd1f9ae67f4aa2
Simone Rizza <simonerizza@mbp-de-simone.home>
2023-05-01 01:54:54 +0200

    build(api): todos

--- src/main/java/com/todo/back/controller/TodoItemController.java
+++ src/main/java/com/todo/back/controller/TodoItemController.java
@@ -6,9 +6,9 @@ import com.todo.back.model.TodoItem;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import com.todo.back.repository.todo.ItemRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.stream.Collectors;
 import java.util.List;
 
@@ -35,4 +35,76 @@ public class TodoItemController {
         return CollectionModel.of(employees, linkTo(methodOn(TodoItemController.class).all()).withSelfRel());
     }
     // end::get-aggregate-root[]
+
+    // tag::get-done-user[]
+    @GetMapping("/todos/TODAY/{userId}")
+    CollectionModel<EntityModel<TodoItem>> allToday(@PathVariable String userId) {
+
+        String today = LocalDate.now().parse("2023-05-01").toString();
+
+        List<EntityModel<TodoItem>> todos = repository.findByUserToday(userId, today).stream()
+                .map(todo -> EntityModel.of(todo,
+                        linkTo(methodOn(TodoItemController.class).all()).withRel("TodoItem")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(todos, linkTo(methodOn(TodoItemController.class).all()).withSelfRel());
+    }
+    // end::get-done-user[]
+
+    // tag::get-inbox-user[]
+    @GetMapping("/todos/INBOX/{userId}")
+    CollectionModel<EntityModel<TodoItem>> allInbox(@PathVariable String userId) {
+
+        List<EntityModel<TodoItem>> todos = repository.findByUserInbox(userId).stream()
+                .map(todo -> EntityModel.of(todo,
+                        linkTo(methodOn(TodoItemController.class).all()).withRel("TodoItem")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(todos, linkTo(methodOn(TodoItemController.class).all()).withSelfRel());
+    }
+    // end::get-inbox-user[]
+
+    // tag::get-done-user[]
+    @GetMapping("/todos/DONE/{userId}")
+    CollectionModel<EntityModel<TodoItem>> allDone(@PathVariable String userId) {
+
+        List<EntityModel<TodoItem>> todos = repository.findByUserDone(userId).stream()
+                .map(todo -> EntityModel.of(todo,
+                        linkTo(methodOn(TodoItemController.class).all()).withRel("TodoItem")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(todos, linkTo(methodOn(TodoItemController.class).all()).withSelfRel());
+    }
+    // end::get-done-user[]
+
+    // tag::post-new-todo[]
+    @PostMapping("/todos")
+    TodoItem newTodo(@RequestBody TodoItem newTodo) {
+
+        return repository.save(newTodo);
+    }
+    // end::post-new-todo[]
+
+    // tag::check-todo[]
+    @PutMapping("/todos")
+    TodoItem replaceTodo(@RequestBody TodoItem newTodo) {
+
+        return repository.findById(newTodo.getId()) //
+                .map(todo -> {
+                    todo.setDone(newTodo.getDone());
+                    return repository.save(todo);
+                }) //
+                .orElseGet(() -> {
+                    newTodo.setDone(newTodo.getDone());
+                    return repository.save(newTodo);
+                });
+    }
+    // end::check-todo[]
+
+    // tag::delete-todo[]
+    @DeleteMapping("/todos/{id}")
+    void deleteEmployee(@PathVariable String id) {
+        repository.deleteById(id);
+    }
+    // end::delete-todo[]
 }
--- src/main/java/com/todo/back/repository/todo/ItemRepository.java
+++ src/main/java/com/todo/back/repository/todo/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.todo.back.repository.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,12 +9,17 @@ import org.springframework.data.mongodb.repository.Query;
 import com.todo.back.model.TodoItem;
 
 public interface ItemRepository extends MongoRepository<TodoItem, String> {
-	
-	@Query("{name:'?0'}")
-	TodoItem findItemByName(String name);
-	
-	@Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
-	List<TodoItem> findAll(String category);
+
+	String today = LocalDate.now().parse("2023-04-30").toString();
+
+	@Query(value="{userId:'?0', done: false, due: ?1}")
+	List<TodoItem> findByUserToday(String userId, String today);
+
+	@Query(value="{userId:'?0', done: false}")
+	List<TodoItem> findByUserInbox(String userId);
+
+	@Query(value="{userId:'?0', done: true}")
+	List<TodoItem> findByUserDone(String userId);
 	
 	public long count();
 
