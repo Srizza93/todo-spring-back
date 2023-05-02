81ecc939608c748f01b5218059c6652197a93f3b
Simone Rizza <simonerizza@mbp-de-simone.home>
2023-05-02 16:42:00 +0200

    build(api): refactor

--- src/main/java/com/todo/back/repository/user/UserRepository.java
+++ src/main/java/com/todo/back/repository/user/UserRepository.java
@@ -4,15 +4,10 @@ import com.todo.back.model.UserProfile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.List;
-
 public interface UserRepository extends MongoRepository<UserProfile, String> {
 
     @Query("{email:'?0'}")
     UserProfile findUserByEmail(String email);
-
-    @Query("{name:'?0'}")
-    List<UserProfile> findAll(String category);
 
     public long count();
 }
