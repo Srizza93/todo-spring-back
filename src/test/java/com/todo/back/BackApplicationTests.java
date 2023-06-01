37f99779c533a5232b6ea7214c29111e84dd2cf3
Simone Rizza <simonerizza@MacBook-Pro-de-Simone.local>
2023-06-01 17:27:58 +0200

    test(unit): smokeTest refactor

--- src/test/java/com/todo/back/BackApplicationTests.java
+++ src/test/java/com/todo/back/BackApplicationTests.java
@@ -1,13 +1,26 @@
 package com.todo.back;
 
+import com.todo.back.controller.TodoItemController;
+import com.todo.back.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BackApplicationTests {
 
+	@Autowired
+	private UserController userController;
+
+	@Autowired
+	private TodoItemController todoItemController;
+
 	@Test
 	void contextLoads() {
+		assertThat(userController).isNotNull();
+		assertThat(todoItemController).isNotNull();
 	}
 
 }
